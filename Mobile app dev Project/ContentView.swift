490f52c8343d73a57395cf492574308bdc26ed6e
Dominik Bosnjak <dxb3501@g.rit.edu>
2020-11-19 14:12:05 +0100

    Pushing initial files

---
+++ Mobile app dev Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mobile app dev Project
+//
+//  Created by Dominik Bosnjak on 19/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mobile app dev Project/Mobile_app_dev_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_app_dev_ProjectApp.swift
+//  Mobile app dev Project
+//
+//  Created by Dominik Bosnjak on 19/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_app_dev_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
